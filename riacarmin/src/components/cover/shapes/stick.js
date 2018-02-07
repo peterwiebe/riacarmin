dc0445b18a0d8676121f8ca37522f24339c60c37
Ria Nicole Carmin <akimalunar@gmail.com>
2018-02-07 14:14:00 -0800

    Stick animation

--- riacarmin/src/components/cover/shapes/stick.js
+++ riacarmin/src/components/cover/shapes/stick.js
@@ -5,21 +5,27 @@ export default class Stick {
     this.speed = 1;
     this.dx = (Math.random() + -0.5);
     this.dy = (Math.random() + -0.5);
-    this.alpha = .8;
+    this.alpha = 1;
+    this.rotation = Math.PI / 4;
     this.ctx = ctx;
   }
 
   init() {
+    this.ctx.save();
     this.ctx.beginPath();
+    this.ctx.translate(this.x, this.y);
+    this.ctx.rotate(this.rotation);
     this.ctx.fillStyle = `rgba(220, 10, 83, ${this.alpha})`;
-    this.ctx.rect(this.x, this.y, 8, 24);
-    this.ctx.fill();
+    this.ctx.fillRect(-4, -12, 8, 24);
+    this.ctx.restore();
+
   }
   draw() {
     this.x += this.dx * this.speed;
     this.y += this.dy * this.speed;
+    this.rotation += .01;
     this.speed = Math.sqrt(this.speed);
-    this.alpha -= .0015;
+    this.alpha -= .0025;
     this.init();
   }
 }
--- riacarmin/src/pages/index.js
+++ riacarmin/src/pages/index.js
@@ -2,12 +2,10 @@ import React from "react";
 import Link from "gatsby-link";
 
 import Cover from "../components/cover";
-import Interests from "../components/interests";
 
 const IndexPage = () => (
   <main className="view">
     <Cover />
-    <Interests />
   </main>
 );
 
